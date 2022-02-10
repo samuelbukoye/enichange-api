6b856d2bacaf8055cff56a043f4c0ff00339475e
Bukoye Samuel <bukoyesamuel9@gmail.com>
2022-02-10 14:26:13 +0100

    a lil edit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
 url = require('mongoose')
 
-// mongoose.connect(process.env.MONGODB_URL)
-mongoose.connect('mongodb://127.0.0.1:27017/simba-api')
+mongoose.connect(process.env.MONGODB_URL)
--- src/models/user.js
+++ src/models/user.js
@@ -102,14 +102,9 @@ userSchema.virtual('transactions', {
 userSchema.methods.generateAuthToken = async function () {
   const user = this
 
-  // const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {
-  const token = jwt.sign(
-    { _id: user._id.toString() },
-    'thisismydopeasssecret',
-    {
-      expiresIn: '7 days'
-    }
-  )
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {
+    expiresIn: '7 days'
+  })
 
   user.tokens = user.tokens.concat({ token })
   await user.save()
